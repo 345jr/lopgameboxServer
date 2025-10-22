1ff0050e1d2ded4ca2219bd7fdb51ebfddd5cc46
Lopop <loppgljr@gmail.com>
2025-10-22 10:33:57 +0800

    fix:添加测试的子域名cors

--- app.ts
+++ app.ts
@@ -15,14 +15,14 @@ app.use(cors({
     // 生产环境域名
     'https://lopbox.lopop.top',
     'https://www.lopbox.lopop.top',
+    'https://test.lopop.top',
     // 开发环境本地地址
     'http://localhost:3000',    // React 默认端口
     'http://localhost:5173',    // Vite 默认端口
     'http://localhost:8080',    // Vue CLI 默认端口
     'http://localhost:4200',    // Angular 默认端口
-    'http://127.0.0.1:5173',    // 本地 IP 形式
+    'http://127.0.0.1:5173',    
     'http://127.0.0.1:3000',
-    // 如果需要允许所有来源，可以设置为 true，但不安全
   ],
   // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   // allowedHeaders: ['','Content-Type', 'Authorization', 'X-Filename'],
